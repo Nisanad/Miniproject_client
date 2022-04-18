051cf9ec3dbe47351b19b7e59454b3e2d838b0d7
Nisanad <s6135512035@phuket.psu.ac.th>
2022-04-18 20:26:26 +0700

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head' 
+import Layout from '../components/layout' 
+import Navbar from '../components/navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home({ token }) {
+ 
+  return (
+    <Layout>
+    <Head>
+        <title>First Page</title>
+    </Head>
+  
+    <div   >
+    <div  >
+        <Navbar />
+       
+        <img
+          src="pizza.jpg"
+          class="w-full"
+          alt="Sample image" 
+       
+        />
+        <h1>11</h1>
+    </div>
+    </div>
+</Layout>
+  )
+}
+
+export function getServerSideProps({ req, res }) {
+  // console.log("token from cookie: ",cookie.get("token")) 
+  // console.log('req: ', req.headers)
+  return { props: { token: req.cookies.token || "" } };
+}
